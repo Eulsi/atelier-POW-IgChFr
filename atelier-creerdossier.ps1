0195c342e51ffcc3fb724dc85e7a372d0faeebf5
IgorMP <mpembele.igor@gmail.com>
2024-10-29 10:53:02 +0100

    Update atelier-creerdossier.ps1

--- atelier-creerdossier.ps1
+++ atelier-creerdossier.ps1
@@ -1,1 +1,13 @@
+[string]$args
 
+if ! (Test-Path -Path $args)
+{
+New-Item -ItemType Directory $args
+Set-Location $args
+New-Item -ItemType Directory Source, Test
+
+
+
+
+
+Write-host "Le dossier existe déja"
